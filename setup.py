54d3946bb25aefbe8fed8ee4d78af8a7cfd5f557
Sebastian Humenda <shumenda |at| gmx |dot| de>
2015-10-09 09:40:08 +0200

    fix installation issue on Windows
    matuc script was installed without extension

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ import os
 import shutil, sys
 
 class my_install(distutils.command.install_scripts.install_scripts):
+    """Custom script installer. Stript .py extension if not on Windows."""
     def run(self):
         distutils.command.install_scripts.install_scripts.run(self)
         for script in self.get_outputs():
-            if script.endswith(".py"):
+            if script.endswith(".py") and not ('wind' in sys.platform or 'win32'
+                    in sys.platform):
                 # strip file ending (if not on windows) to make it executable as
                 # a command
                 shutil.move(script, script[:-3])
