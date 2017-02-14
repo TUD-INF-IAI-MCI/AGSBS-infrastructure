b930661817a9ef880725184bdc84fc4d3878ef8e
jevo <jens781@googlemail.com>
2017-02-14 14:18:33 +0100

    test with correct path

--- tests/test_pagenumber.py
+++ tests/test_pagenumber.py
@@ -10,6 +10,7 @@ class test_pagenumber(unittest.TestCase):
     def test_that_pagenumber_before_line_number_is_found(self):
         content = {1:['|| - Seite 1 -'], 2:['Text'], 3:['|| Text'], 4:['|| - Seite 2 -']}
         pagenum = pm.get_page_number(os.path.realpath("test_pagenumber.md"), 3)
+        pagenum = pm.get_page_number(os.path.realpath("tests/test_pagenumber.md"), 3)
         self.assertEqual(2, pagenum)
 
     
