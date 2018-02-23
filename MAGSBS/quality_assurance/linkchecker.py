5b35cc4173809d401634081be18b2585225e1eec
xplhak <xplhak@fi.muni.cz>
2018-02-23 16:29:02 +0100

    add TODOs and angle brackets support

--- MAGSBS/quality_assurance/linkchecker.py
+++ MAGSBS/quality_assurance/linkchecker.py
@@ -19,10 +19,11 @@ those for the link checker.
 import os
 import re
 
-INLINE_LINK = re.compile(r'\[([^\]]+)\]\(([^)^"]+)\)')
-INLINE_LINK_WITH_TITLE = re.compile(r'\[([^\]]+)\]\(([^)]+("|\')[^)]+)\)')
-# FOOTNOTE_LINK_TEXT_RE = re.compile(r'\[([^\]]+)\]\[(\d+)\]')
-# FOOTNOTE_LINK_URL_RE = re.compile(r'\[(\d+)\]:\s+(\S+)')
+INLINE_LINK = re.compile(r'\[([^\]]+)\]\s*\(([^)^"]+)\)')
+INLINE_LINK_WITH_TITLE = re.compile(r'\[([^\]]+)\]\s*\(([^)]+("|\')[^)]+)\)')
+FOOTNOTE_LINK_TEXT = re.compile(r'\[([^\]]+)\]\s*\[(.+)\]')
+FOOTNOTE_LINK_URL = re.compile(r'\[(.+)\]:\s*(\S+)')
+ANGLE_BRACKETS_LINK = re.compile(r'<\S+>')
 
 """
 def_links = re.compile(
@@ -32,17 +33,16 @@ def_links = re.compile(
 )
 """
 
-
 """
-A common source of error are broken links. Normal link checkers won't work, 
-since they are working on HTML files. It is hence necessary to parse all 
-MarkDown links and implement the destination checks manually (to the 
-resulting HTML files). As a plus, references within the document could be 
+A common source of error are broken links. Normal link checkers won't work,
+since they are working on HTML files. It is hence necessary to parse all
+MarkDown links and implement the destination checks manually (to the
+resulting HTML files). As a plus, references within the document could be
 checked as well.
 
-For checking IDs, it is a good idea to generate IDs of the target document. 
+For checking IDs, it is a good idea to generate IDs of the target document.
 Headings get automatic IDs, which can be generated using datastructures.gen_id.
-Furthermore, the user may create own anchors with <span id="foo"/> or the 
+Furthermore, the user may create own anchors with <span id="foo"/> or the
 div equivalent.
 """
 
@@ -52,7 +52,7 @@ Checking links in the markdown document
 - b) test if they are corretly structured
 - c) check external links
     - ca) test if the computer is online
-    - cb) if online - test the reachability of the 
+    - cb) if online - test the reachability of the link
 - d) check internal links (no need to be online, files should be on the disk)
     - da) check if files are generated
     - db) if they are - check all links given by markdown
@@ -69,13 +69,9 @@ class LinkParser():
         self.file_tree = file_tree  # files to be examined
         self.links_list = []
 
-
     def get_list_of_md_files(self):
-        """
-        This function creates list of paths to .md files which links should be
-        tested.
-        :return: List of paths to .md files to be tested
-        """
+        """ This function creates list of paths to .md files which links should
+        be tested. It also returns the name of the file. """
         md_file_list = []
         for directory_name, dir_list, file_list in self.file_tree:
             for file in file_list:
@@ -89,46 +85,51 @@ class LinkParser():
 
     def extract_links(self):
         for file_path, file_name in self.get_list_of_md_files():
-            # encoding should be already checked
+            # encodingshould be already checked
             with open(file_path, encoding="utf-8") as f:
                 # call the function for finding links
                 self.find_md_links(f, file_name)
-        print(self.links_list) # TODO: remove this testing string
+        print(self.links_list)  # TODO: remove this testing string
 
     def find_md_links(self, md_file_data, file_name):
         """ Updates the list of dictionaries that contains the links retrieved
-        from the markdown string
-        """
-
+        from the markdown string. """
         for line_i, line in enumerate(md_file_data, 1):
-            for reg_expr in ["INLINE_LINK", "INLINE_LINK_WITH_TITLE"]:
+            # for reg_expr in ["INLINE_LINK", "INLINE_LINK_WITH_TITLE"]:
+            #for reg_expr in ["FOOTNOTE_LINK_TEXT", "FOOTNOTE_LINK_URL"]:
+            # for reg_expr in ["TEXT_LINK_ITSELF"]:
+            for reg_expr in ["ANGLE_BRACKETS_LINK"]:
                 # detect links using regular expressions
                 links = eval(reg_expr + ".findall(line)")
                 # links to be have at least two information - link heading and
-                # link itself - otherwise it not be checked
-                if links and len(links) > 0 and len(links[0]) > 1:
+                # link itself - otherwise it is not checked
+                if links:
                     self.links_list.append(self.create_dct(file_name, reg_expr,
                                                            line_i, links))
 
-            # footnote_links = dict(FOOTNOTE_LINK_TEXT_RE.findall(line))
-            # footnote_urls = dict(FOOTNOTE_LINK_URL_RE.findall(line))
-
-        # for key, value in footnote_links.items():
-        #    footnote_links[key] = footnote_urls[value]
-        # links.update(footnote_links)
-
+        # TODO: parsing if link is written over one line
+        # TODO: if the link is with the title
+                    # check if it is correctly build using " or ' (the last
+                    # char should be the there at least twice - and first one
+                    # is the end of the link
+        # TODO: detect also TEXT_LINK_ITSELF - link only in []
+                    # error if it has no reference in footnote_link_url
+        # TODO: link within picture description
 
     def create_dct(self, file_name, type, line_no, links):
-        """
-        This function generates the dictionary that contains all the important
-        data for the link
-        """
+        """ This function generates the dictionary that contains all the
+        important data for the link """
         link_dictionary = {}
         link_dictionary["file"] = file_name
         link_dictionary["type"] = type.lower()
         link_dictionary["line_no"] = line_no
-        link_dictionary["link_title"] = links[0][0]  # get title
-        link_dictionary["link"] = links[0][1]  # get link itself
+        if isinstance(links[0], str):  # its only the angle_brackets or text_link_itself
+            link_dictionary["link"] = links[0][1:-1]
+        else:  # the result has two parts
+            link_dictionary["link_title"] = links[0][0]  # title
+            link_dictionary["link"] = links[0][1]  # link itself
+            link_dictionary["link"] = links[0][1]  # link itself
+
         return link_dictionary
 
     def target_exists(self, target_file_name):
@@ -138,11 +139,10 @@ class LinkParser():
 class LinkStructureChecker():
     pass
 
+
 class LinkOnlineChecker():
     pass
 
+
 class RelativeUrlChecker():
     pass
-
-
-
