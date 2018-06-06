8ca7b1a002f786e4db7d745deb2b1f1ee35c7791
Sebastian Humenda <shumenda@gmx.de>
2018-06-06 14:20:51 +0200

    first draft of gleetex library integration

--- MAGSBS/pandoc/contentfilter.py
+++ MAGSBS/pandoc/contentfilter.py
@@ -1,39 +1,38 @@
 # This is free software, licensed under the LGPL v3. See the file "COPYING" for
 # details.
 #
-# (c) 2016-2017 Sebastian Humenda <shumenda |at|gmx |dot| de>
+# (c) 2016-2018 Sebastian Humenda <shumenda |at|gmx |dot| de>
 #pylint: disable=unused-argument
 """
-This module provides some extensions to the python-pandoc API as well as a few
-custom filters for the extended version of MarkDown.
-Additionally, a parser is introduced for documents with LaTeX formulas. This one
-pre-parses a document and changes all inline equations to displaymath equations.
-This way Pandoc preserves the line breaks which ware curcial for alternative
-image descriptions.
-"""
+To convert lecture material and books taillored to our requirements, content
+filters have been written, stored in this module. A content filter is a function
+(or program) working on the abstract document representation of a Pandoc
+document and extracting, removing or alterint the AST."""
 
 import json
 import subprocess
 import sys
+
+import gleetex
 import pandocfilters
+
 from .. import config
 from ..errors import SubprocessError
 
 
 html = lambda text: pandocfilters.RawBlock('html', text)
 
-
 def join_para(chunks):
     """join_para(chunks)
 Pandoc's json is recursively nested. To search for a particular string (like we
-        do for page numbers), one has to join all the nested text chunks."""
-    str = ''
+do for page numbers), all text chunks have to be joined."""
+    string = ''
     for chunk in chunks:
         if isinstance(chunk['c'], list) and chunk['t'] == 'Space':
-            str += ' '
+            string += ' '
         else:
-            str += chunk['c']
-    return str
+            string += chunk['c']
+    return string
 
 def page_number_extractor(key, value, fmt, meta):
     """Scan all paragraphs for those starting with || to parse it for page
@@ -70,31 +69,31 @@ def suppress_captions(key, value, fmt, meta, modify_ast=True):
         # vanishes:
         value[-1][1] = ''
 
-def heading_extractor(key, value, format, meta, modify_ast=False):
+def heading_extractor(key, value, fmt, meta, modify_ast=False):
     """Extract all headings from the JSon AST."""
-    if(key == 'Header'):
+    if key == 'Header':
         # value[0] is the heading level
-        return (value[0], pandocfilters.stringify( value ))
+        return (value[0], pandocfilters.stringify(value))
 
 
 def file2json_ast(file_name):
     """Read in specified file and return a JSON AST."""
-    with open(file_name, encoding='utf-8') as f:
-        return text2json_ast(f.read())
+    with open(file_name, encoding='utf-8') as file:
+        return text2json_ast(file.read())
 
 def text2json_ast(text):
     """Run pandoc and return parsed JSON AST."""
-    command = ['pandoc', '-f','markdown', '-t','json']
+    command = ['pandoc', '-f', 'markdown', '-t', 'json']
     proc = subprocess.Popen(command, stdin=subprocess.PIPE,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     data = proc.communicate(text.encode(sys.getdefaultencoding()))
     ret = proc.wait()
     if ret:
-        error = '\n'.join([e.decode( sys.getdefaultencoding() )
+        error = '\n'.join([e.decode(sys.getdefaultencoding())
                 for e in data])
         raise SubprocessError(' '.join(command),
         "Pandoc gave error status %s: %s" % (ret, error))
-    text = data[0].decode( sys.getdefaultencoding() )
+    text = data[0].decode(sys.getdefaultencoding())
     return json.loads(text)
 
 
@@ -105,10 +104,10 @@ def json_ast_filter(doc, action):
     if not isinstance(doc, (dict, list)):
         raise TypeError("A JSON AST is required, got %s" % type(doc))
     result = []
-    def go(key, value, format, meta):
-        res = action(key, value, format, meta, modify_ast=False)
-        if(res):
-            result.append( res )
+    def go(key, value, fmt, meta):
+        res = action(key, value, fmt, meta, modify_ast=False)
+        if res:
+            result.append(res)
     pandocfilters.walk(doc, go, "", doc['blocks'])
     return result
 
@@ -135,7 +134,7 @@ class Formula:
 
 
 def get_title(json_ast):
-    nodes_to_visit = json_ast['blocks'] # content of document without meta information
+    nodes_to_visit = json_ast['blocks'] # content of document without meta info
     for node in nodes_to_visit:
         if isinstance(node, list):
             nodes_to_visit.extend(node)
@@ -144,4 +143,22 @@ def get_title(json_ast):
             if 't' in node and node.get('t') == 'Header' and 'c' in node:
                 if node['c'][0] == 1: # level 1 heading
                     return pandocfilters.stringify(node['c'])
+
+def convert_formulas(base_path, ast):
+    """This filter extracts all formulas from the given Pandoc AST, converts
+    them and replaces their original occurrences with RAW inline HTML."""
+    formulas = gleetex.pandoc.extract_formulas(ast)
+    conv = gleetex.cachedconverter.CachedConverter(base_path, True,
+            encoding="UTF-8")
+    conv.set_replace_nonascii(True)
+    conv.convert_all(formulas)
+    # an converted image has information like image depth and height and hence
+    # the data structure is different
+    formulas = [conv.get_data_for(eqn, style) for _p, style, eqn in formulas]
+    with gleetex.htmlhandling.HtmlImageFormatter(base_path)  as img_fmt:
+        img_fmt.set_exclude_long_formulas(True)
+        img_fmt.set_replace_nonascii(True)
+        # this alters the AST reference, so no return value required
+        gleetex.pandoc.replace_formulas_in_ast(img_fmt, ast['blocks'],
+                formulas)
 
